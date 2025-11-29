8c1122759fe946a0fea266d500a60ea07e4e5a2a
Sourav Roy <1994sourav@gmail.com>
2025-11-29 15:11:31 +0530

    KotlinOptions and Gradle update
    Signed-off-by: Sourav Roy <1994sourav@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
 	}
-    kotlin {
-        jvmToolchain(8)
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 	buildFeatures {
 		compose = true
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "CleanProductApp"
 include(":app")
- 
